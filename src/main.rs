1295227d22ecbc5da0676b0c27120efd02d58333
Peter <peter.liebau@gmx.de>
2024-05-17 10:14:51 +0200

    neu

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
-    let a: i32 = 42;
-    println!("Hallo, Welt! {}", a);
+    println!("What is your name?");
+    let input = read_string();
+    println!("Hello: {}", input);
+}
+
+fn read_string() -> String {
+    let mut eingabe = String::new();
+    std::io::stdin()
+        .read_line(&mut eingabe)
+        .expect("can not read user eingabe");
+    eingabe
 }

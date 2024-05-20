2bbed099b153bf7b334b7d6a2a95f418c00da4f6
Peter <peter.liebau@gmx.de>
2024-05-20 16:52:46 +0200

    edit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
+/*---------------------------------------------------------------------
+--   ____  _     
+--  |  _ \| |      Peter Liebau
+--  | |_) | |      17.05.2024
+--  |  __/| |___   
+--  |_|   |_____|  
+---------------------------------------------------------------------*/
+
+use chrono::{DateTime, Local};
+// use std::fs::File;
+
+// ( main )------------------------------------------------------------
+
 fn main() {
-    println!("What is your name?");
+
+// https://rustjobs.dev/blog/date-time-formatting-in-rust/
+    let current_local: DateTime<Local> = Local::now();
+    // let custom_format = current_local.format("%d.%m.%Y %H:%M:%S");
+    let custom_date = current_local.format("%d. %B %Y");
+    let custom_time = current_local.format("%H:%M:%S");  
+    println!("Heute ist der {} und es ist {} Uhr.", custom_date, custom_time); 
+
+    let custom_dat = current_local.date_naive(); 
+    let custom_ti = current_local.time();
+    println!("{}", custom_dat);
+    println!("{}", custom_ti);
+
+    // let mut file = File::create("foo.txt");
+    // file.write_all(b"Hello, world!");
+
+/*    println!("Wie heisst Du?");
     let input = read_string();
-    println!("Hallo {}", input);
+    println!("Hallo {}", input);*/
 }
 
-fn read_string() -> String {
+/*fn read_string() -> String {
     let mut eingabe = String::new();
     std::io::stdin()
         .read_line(&mut eingabe)
         .expect("can not read user eingabe");
     eingabe
 }
+*/

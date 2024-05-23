627f731527951f2aeb101c863ec9364871ccc819
LuckLive <peter.liebau@gmx.de>
2024-05-23 12:04:52 +0200

    edit

--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,18 @@ fn main() {
     let zeit: DateTime<Utc> = Utc.timestamp_opt(timestamp_berlin as i64, 0).unwrap();
     let zeitform = zeit.format("%H:%M:%S");
     println!("Sonnenaufgang {} Uhr", zeitform);
+    
 // https://rustjobs.dev/blog/date-time-formatting-in-rust/
-let current_local: DateTime<Local> = Local::now();
+    let current_local: DateTime<Local> = Local::now();
     // let custom_format = current_local.format("%d.%m.%Y %H:%M:%S");
     let custom_date = current_local.format("%d. %B %Y");
     let custom_time = current_local.format("%H:%M:%S");  
-    println!("Heute ist der {} und es ist {} Uhr.", custom_date, custom_time); 
+    println!("Heute ist der {} und es ist {} Uhr.", custom_date, custom_time);
 
-    let custom_dat = current_local.date_naive(); 
-    let custom_ti = current_local.time();
-    println!("{}", custom_dat);
-    println!("{}", custom_ti);
+    // let custom_dat = current_local.date_naive(); 
+    // let custom_ti = current_local.time();
+    // println!("{}", custom_dat);
+    // println!("{}", custom_ti);
 
     // let mut file = File::create("foo.txt");
     // file.write_all(b"Hello, world!");
